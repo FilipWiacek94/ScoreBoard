bb50ed3f91d78be3bf7f513e40efdf4c0239aa99
FilipW <filipwiacek94@gmail.com>
2024-04-17 11:14:36 +0200

    Implemented test for FootballMatchProcessor in case of update score.

--- src/test/java/processor/FootballMatchProcessorTest.java
+++ src/test/java/processor/FootballMatchProcessorTest.java
@@ -1,5 +1,6 @@
 package processor;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.example.model.FootballMatch;
 import org.example.processor.FootballMatchProcessor;
 import org.example.processor.impl.FootballMatchProcessorImpl;
@@ -24,5 +25,25 @@ public class FootballMatchProcessorTest {
         assertEquals(startedFootballMatch.getAwayTeamName(), AWAY_TEAM);
         assertEquals(startedFootballMatch.getAwayTeamScore(), Integer.valueOf(0));
         assertEquals(startedFootballMatch.getHomeTeamScore(), Integer.valueOf(0));
+    }
+
+    @Test
+    public void shouldUpdateFootballMatchScore() {
+        //given
+        FootballMatchProcessor processor = new FootballMatchProcessorImpl();
+        processor.startFootballMatch(HOME_TEAM, AWAY_TEAM);
+
+        Pair<String, Integer> homeTeamNewScore = Pair.of(HOME_TEAM, 2);
+        Pair<String, Integer> awayTeamNewScore = Pair.of(AWAY_TEAM, 1);
+
+        //when
+        FootballMatch updatedMatch = processor.updateMatchScore(homeTeamNewScore, awayTeamNewScore);
+
+        //then
+        assertEquals(updatedMatch.getHomeTeamName(), HOME_TEAM);
+        assertEquals(updatedMatch.getAwayTeamName(), AWAY_TEAM);
+        assertEquals(updatedMatch.getHomeTeamScore(), Integer.valueOf(2));
+        assertEquals(updatedMatch.getAwayTeamScore(), Integer.valueOf(1));
+        assertEquals(updatedMatch.getTotalScore(), Integer.valueOf(3));
     }
 }
