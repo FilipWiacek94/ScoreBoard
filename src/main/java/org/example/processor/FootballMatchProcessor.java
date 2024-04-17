c102f6bad80603c082a56542b9df531429cdbb63
FilipW <filipwiacek94@gmail.com>
2024-04-17 11:24:22 +0200

    Implemented logic for FootballMatchProcessor in case of update score.

--- src/main/java/org/example/processor/FootballMatchProcessor.java
+++ src/main/java/org/example/processor/FootballMatchProcessor.java
@@ -1,15 +1,24 @@
 package org.example.processor;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.example.model.FootballMatch;
 
 public interface FootballMatchProcessor {
 
     /**
-     * Method responsible for creating new football match and storing it in the database.
+     * Method responsible for creating new football match.
      * @param homeTeam Name of a home team.
-     * @param awayTeam Name of a away team.
+     * @param awayTeam Name of an away team.
      * @return Newly created FootballMatch object.
      */
     FootballMatch startFootballMatch(String homeTeam, String awayTeam);
+
+    /**
+     * Method responsible for updating score of the football match.
+     * @param homeTeamNewScore Object containing name of a home team and a new score.
+     * @param awayTeamNewScore Object containing name of an away team and a new score.
+     * @return Updated football match object.
+     */
+    FootballMatch updateMatchScore(Pair<String, Integer> homeTeamNewScore, Pair<String, Integer> awayTeamNewScore);
 }
 
--- src/main/java/org/example/processor/impl/FootballMatchProcessorImpl.java
+++ src/main/java/org/example/processor/impl/FootballMatchProcessorImpl.java
@@ -1,5 +1,6 @@
 package org.example.processor.impl;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.example.model.FootballMatch;
 import org.example.processor.FootballMatchProcessor;
 import org.example.repository.FootballMatchRepository;
@@ -17,5 +18,10 @@ public class FootballMatchProcessorImpl implements FootballMatchProcessor {
     public FootballMatch startFootballMatch(String homeTeam, String awayTeam) {
         FootballMatch newFootballMatch = FootballMatch.createNewFootballMatch(homeTeam, awayTeam);
         return footballMatchRepository.startFootballMatch(newFootballMatch);
+    }
+
+    @Override
+    public FootballMatch updateMatchScore(Pair<String, Integer> homeTeamNewScore, Pair<String, Integer> awayTeamNewScore) {
+        return footballMatchRepository.updateFootballMatch(homeTeamNewScore, awayTeamNewScore);
     }
 }
